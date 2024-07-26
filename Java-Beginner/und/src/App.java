dbd4708eb779e5a8341d38159ac258bf283afc25
per5cm <per5cm@proton.me>
2024-07-26 16:17:56 +0200

    bump

---
+++ Java-Beginner/und/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //boolean a = true;
+        //boolean b = false;
+
+        //System.out.println (a | b);
+        //System.out.println("kurzes logishes UND" + (a && b));
+        //System.out.println("kurzes logishes ODER" + (a || b));
+        //System.out.println("Logisches EXKLUSIV-ODER" + (a^ b));
+        // System.out.println ("logisches Nicht (Negation) " +!a);
+        // System.out.println (!b);
+        // System.out.println ("Logishes UND " + (a & b));
+        // System.out.println ("Logisches ODER" +(a | b));
+
+        //int x = 1;
+        //int y = 2;
+
+        //System.out.println(x > y);
+        //System.out.println(x < y);
+        //System.out.println(x == y);
+        //System.out.println(x >= y);
+        //System.out.println(x <= y);
+        //System.out.println(x != y);
+
+        int x = 353;
+
+        if (x % 2 == 0) {
+            System.out.println("Zahl ist gerade");
+
+        }
+    }
+}

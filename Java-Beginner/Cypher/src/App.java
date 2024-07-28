e755b2f833a52c4ee446291256377d4ab81e76d1
per5cm <per5cm@proton.me>
2024-07-28 17:30:44 +0200

    bump

--- Java-Beginner/Cypher/src/App.java
+++ Java-Beginner/Cypher/src/App.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n;
+        
+        // do while method when n is less than 1, while n is greater than 25.
+        do {
+            System.out.print("Enter a number from 1 to 25: ");
+            n = scanner.nextInt();
+        } while (n < 1 || n > 25);
+        scanner.nextLine();
 
+        // shift value and your message
         System.out.print("Would you like to encrypt or decrypt a message? (e/d): ");
         String choice = scanner.nextLine().toLowerCase();
+        
 
         System.out.print("Enter the shift value: ");
         int shift = scanner.nextInt();
@@ -13,7 +23,9 @@ public class App {
 
         System.out.print("Enter your message: ");
         String message = scanner.nextLine();
+        
 
+        // choice if e pressed else if d
         if (choice.equals("e")) {
             String encryptedMessage = encrypt(message, shift);
             System.out.println("Encrypted message: " + encryptedMessage);
@@ -26,7 +38,7 @@ public class App {
 
         scanner.close();
     }
-
+    // ceasars cypher starts here.
     public static String encrypt(String message, int shift) {
         StringBuilder encryptedMessage = new StringBuilder();
 
